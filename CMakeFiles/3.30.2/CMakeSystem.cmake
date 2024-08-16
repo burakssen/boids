c982a98c2ce0973658455c31b19bfc095c25e8c8
burakssen <43742977+burakssen@users.noreply.github.com>
2024-08-16 05:28:54 +0000

    Deploying to gh-pages from @ burakssen/boids@b128b63e9ad770617b0bac94f6ee3bab5fd4fda1 🚀

---
+++ CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.5.0-1025-azure")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-1025-azure")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/runner/work/_temp/0b406434-c3a4-4e24-a42f-248d6c56fc4b/emsdk-main/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+
+set(CMAKE_SYSTEM "Emscripten-1")
+set(CMAKE_SYSTEM_NAME "Emscripten")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ vendor/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/runner/work/boids/boids/vendor/raylib/src
+# Build directory: /home/runner/work/boids/boids/build/vendor/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
